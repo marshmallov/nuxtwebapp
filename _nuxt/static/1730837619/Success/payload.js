432da8a97d75150ff1ce677133dc5393977c15b5
marshmallov <mar.s.hmallov@outlook.com>
2024-11-05 21:15:06 +0100

    first try

---
+++ _nuxt/static/1730837619/Success/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/Success", {data:[{}],fetch:{},mutations:[]});
--- store/index.js
+++
@@ -1,6 +0,0 @@
-import { reactive } from 'vue';
-
-export const State = reactive({
-    isPlaying: false,
-    isHomePage: true, 
-  });
